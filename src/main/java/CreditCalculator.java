4849f2a09f3dad1e16ce36a406850c6fcba79bb9
Петр Петров <3033106@gmail.com>
2022-07-02 19:39:33 +0400

    commit changes

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,13 +1,15 @@
 public class CreditCalculator {
 
-    public int monthPay(int amountMonth, int money, double interestRate) { return  0;}
-
-    public int sumMoney(int amountMonth, int money, double interestRate) {
-        return 0;
+    public int monthPay(int month, int money, double interestRate) {
+        return  (int) Math.round  ((((((money * interestRate) / 12.0) * month) + money) / month));
     }
 
-    public int overpayment(int amountMonth, int money, double interestRate) {
-        return 0;
+    public int sumMoney(int month, int money, double interestRate) {
+        return (int) Math.round  (((((money * interestRate) / 12.0) * month) + money));
+    }
+
+    public int overpayment(int month, int money, double interestRate) {
+        return (int) Math.round  ((((money * interestRate) / 12.0) * month));
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int fff = 1000;
-        double ggg = 0.14;
-        int mmmm = (int) (fff * ggg);
-        double gggl  = (400000 * 0.19 /12.0 * 35) + 400000;
-        System.out.println(Math.round(82666.66666666666));
+
     }
 }
